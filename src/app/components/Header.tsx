ed3459b13dd94260c88984bcfd4255b5f76fded5
nashdev255 <nashdev255@gmail.com>
2024-01-01 18:49:32 +0900

    Decide not to use MUI

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,8 @@
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 const Header = () => {
   return (
     <header>
-      <AppBar>
-        <Toolbar>
-          <Typography></Typography>
-          <Button></Button>
-          <Button></Button>
-        </Toolbar>
-      </AppBar>
+
     </header>
   );
 };
