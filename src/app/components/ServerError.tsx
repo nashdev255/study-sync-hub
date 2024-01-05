294482896dfe49b35185e5ed716d6b7d9d9ef042
nashdev255 <nashdev255@gmail.com>
2024-01-05 22:34:00 +0900

    Add component ServerError

---
+++ src/app/components/ServerError.tsx
@@ -0,0 +1,10 @@
+const ServerError = () => {
+  return (
+    <div>
+      <div className="mb-3 text-center text-5xl font-bold">500</div>
+      <div className="text-center text-xl font-bold">Server Error</div>
+    </div>
+  );
+};
+
+export default ServerError;
