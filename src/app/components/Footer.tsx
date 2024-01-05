3b3fd4dff50414718f13d5528588e3f29472e37d
nashdev255 <nashdev255@gmail.com>
2024-01-05 21:31:22 +0900

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className='bg-slate-300'>
+      <div className='flex justify-between mx-12 py-6'>
+        <span className=''>{ new Date().getFullYear() } &copy; All Rights Reserved</span>
+        <div>
+            Build with <Link href={'https://github.com/nashdev255/'}>nashdev255</Link>
+        </div>
+      </div>
     </footer>
   );
 };
