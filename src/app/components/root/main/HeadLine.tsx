7b295695f9dec2a528e8b13fba818b20e2937a53
nashdev255 <nashdev255@gmail.com>
2024-01-13 11:33:19 +0900

    Update HeadLine

--- src/app/components/root/main/HeadLine.tsx
+++ src/app/components/root/main/HeadLine.tsx
@@ -4,17 +4,17 @@ import Image from 'next/image';
 const HeadLine = () => {
   return (
     <>
-      <div className="inline bg-[url('/images/header.png')] bg-cover h-auto sm:flex justify-center sm:space-x-12 py-6">
+      <div className="inline bg-[url('/images/header.png')] bg-cover sm:flex justify-center sm:space-x-12 xl:py-36">
         <div className='flex justify-center'>
           <div className='h-auto w-auto py-10 items-center'>
             <Image src={'/images/ssh_fa.png'} alt='StudySyncHub' height={'200'} width={'200'} />
           </div>
         </div>
         <div className='my-auto h-full items-center space-y-4'>
-          <div className='items-center text-center text-4xl font-bold text-gray-200'>
-            <h1>学びに「同期」と</h1>
-            <h1>よりよい学習体験を</h1>
-            <h1>みんなで創る</h1>
+          <div className='items-center text-center text-4xl font-bold text-gray-200 space-y-2'>
+            <h1><span className="px-3 bg-gray-700">学びに「同期」と</span></h1>
+            <h1><span className="px-3 bg-gray-700">よりよい学習体験を</span></h1>
+            <h1><span className="px-3 bg-gray-700">みんなで創る</span></h1>
           </div>
           <div className='text-center'>
             <Link href={'/auth/signup'}>
