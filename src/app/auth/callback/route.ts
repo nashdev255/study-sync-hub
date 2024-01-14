3ae85b5e0b41ef12434534d569318ca662b84853
nashdev255 <nashdev255@gmail.com>
2024-01-14 17:57:06 +0900

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,17 +1,20 @@
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import { Database } from '@/lib/database.types';
+
+import type { NextRequest } from 'next/server';
+import type { Database } from '@/lib/database.types';
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  
-  if ( code ) {
-    const supabase = createRouteHandlerClient<Database>({ cookies });
+
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
+  // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
