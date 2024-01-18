b3b308f6abae762bef67b4042248ff815f5229bc
nashdev255 <nashdev255@gmail.com>
2024-01-18 21:26:59 +0900

    Update create_progresses.sql

--- public/create_progresses.sql
+++ public/create_progresses.sql
@@ -1,13 +1,14 @@
--- subjects type declaration
-create type types.subjects as enum (
-  '国語', '数学', '英語'
+-- subjects table declaration
+create table if not exists public.subjects (
+  id serial primary key,
+  name text not null unique
 );
 
 -- progresses table declaration
 create table if not exists public.progresses (
   id uuid not null primary key default uuid_generate_v4(),
   user_id uuid not null references auth.users(id) default auth.uid(),
-  subject types.subjects,
+  subject_id int references public.subjects(id),
   content text,
   created_at timestamp with time zone default timezone('utc' :: text, now()) not null
 );
