0bbc7b8e04e49e9079f894c927b5c7cde954f47f
nashdev255 <nashdev255@gmail.com>
2024-03-10 16:20:09 +0900

    Add Progress Modal

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { IoIosClose, IoIosPaper } from 'react-icons/io';
 
 const Progress = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
@@ -16,12 +16,28 @@ const Progress = () => {
               <p className='text-xl font-bold text-slate-300'>まだ進捗を生んでいませんか？</p>
               <IoIosPaper size={250} color={'white'} />
               <div className='rounded-full bg-[#0d9ddd] px-6 py-2 hover:cursor-pointer hover:bg-[#0987bc]'>
-                <p className='text-lg font-bold text-white'>新しく生やす</p>
+                <button onClick={() => { setIsModalOpen(true); }}>
+                  <p className='text-lg font-bold text-white'>新しく生やす</p>
+                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {isModalOpen ? (
+        <div className='absolute left-1/2 top-1/2 h-[80vh] w-[60vw] translate-x-[-50%] translate-y-[-50%] rounded-lg bg-white shadow-lg'>
+          <div className='flex justify-end p-2'>
+            <button
+              className='rounded-md hover:bg-slate-500 hover:bg-opacity-[0.30]'
+              onClick={() => { setIsModalOpen(false); }}
+            >
+              <IoIosClose size={'32'} />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
