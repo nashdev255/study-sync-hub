2da8987663f305c3f6a719bb114d1da1f01040d7
nashdev255 <nashdev255@gmail.com>
2024-03-10 15:18:34 +0900

    Update Progress

--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -1,43 +1,26 @@
 'use client';
 
 import { useState } from 'react';
-import { Button, Container } from '@mui/material';
-import Modal from 'react-modal';
-
-const customStyles = {
-  content: {
-    top: '20%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    minWidth: '40%',
-  },
-};
+import { IoIosClose, IoIosPaper } from 'react-icons/io';
 
 const Progress = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <>
-      <div className='w-full justify-center'>
-        <Container maxWidth="sm">
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              setModalIsOpen(true);
-            }}
-          >
-            <h2 className='font-bold'>
-              進捗を生やす！！！
-            </h2>
-          </Button>
-          <Modal isOpen={modalIsOpen} style={customStyles}>
-            
-          </Modal>
-        </Container>
+      <div className='mb-8 flex justify-center'>
+        <div className='w-[80vw]'>
+          <h1 className='m-12 text-4xl font-bold'>進捗の管理</h1>
+          <div className='flex justify-center'>
+            <div className='flex flex-col items-center space-y-2'>
+              <p className='text-xl font-bold text-slate-300'>まだ進捗を生んでいませんか？</p>
+              <IoIosPaper size={250} color={'white'} />
+              <div className='rounded-full bg-[#0d9ddd] px-6 py-2 hover:cursor-pointer hover:bg-[#0987bc]'>
+                <p className='text-lg font-bold text-white'>新しく生やす</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
