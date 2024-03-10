078ca3f1686887a8bfc134b980c7af9c99c89c68
nashdev255 <nashdev255@gmail.com>
2024-03-10 14:12:16 +0900

    Navigation hover

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -30,7 +30,7 @@ const Navigation = () => {
       <nav className='flex w-full justify-center'>
         <ul className='flex space-x-4 text-lg font-bold text-white md:text-xl'>
           {options.map((item, index) => (
-            <Link key={index} href={item.href} className={`${pathname === item.href && 'border-b-2 border-white'} py-2 md:px-8`}>
+            <Link key={index} href={item.href} className={`${pathname === item.href && 'border-b-2 border-white'} py-2 hover:border-b-2 hover:bg-slate-500 hover:bg-opacity-[0.30] md:px-8`}>
               {item.name}
             </Link>
           ))}
--- src/app/components/Progress.tsx
+++ src/app/components/Progress.tsx
@@ -1,6 +1,45 @@
+'use client';
+
+import { useState } from 'react';
+import { Button, Container } from '@mui/material';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '20%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    minWidth: '40%',
+  },
+};
+
 const Progress = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
-    <></>
+    <>
+      <div className='w-full justify-center'>
+        <Container maxWidth="sm">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              setModalIsOpen(true);
+            }}
+          >
+            <h2 className='font-bold'>
+              進捗を生やす！！！
+            </h2>
+          </Button>
+          <Modal isOpen={modalIsOpen} style={customStyles}>
+            
+          </Modal>
+        </Container>
+      </div>
+    </>
   );
 };
 
